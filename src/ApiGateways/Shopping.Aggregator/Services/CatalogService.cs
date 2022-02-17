de037f71a4629a89e42910cc75e814b7ee53893a
Ernest Okosobo <okosobo.ernest@gmail.com>
2022-02-17 19:25:13 +0100

    Adding shopping site

---
+++ src/ApiGateways/Shopping.Aggregator/Services/CatalogService.cs
@@ -0,0 +1,39 @@
+﻿using Shopping.Aggregator.Extensions;
+using Shopping.Aggregator.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Services
+{
+    public class CatalogService : ICatalogService
+    {
+        private readonly HttpClient e_Client;
+
+        public CatalogService(HttpClient httpClient)
+        {
+            e_Client = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalog()
+        {
+            var response = await e_Client.GetAsync("/api/v1/Catalog");
+
+            return await response.ReadContentAs<List<CatalogModel>>();
+        }
+
+        public async Task<CatalogModel> GetCatalog(string id)
+        {
+            var response = await e_Client.GetAsync($"/api/v1/Catalog/{id}");
+
+            return await response.ReadContentAs<CatalogModel>();
+        }
+
+        public async Task<IEnumerable<CatalogModel>> GetCatalogByCategory(string category)
+        {
+            var response = await e_Client.GetAsync($"/api/v1/Catalog/GetProductByCategory/{category}");
+            return await response.ReadContentAs<List<CatalogModel>>();
+        }
+    }
+}
